1125b6a094a8e5decf0dc26f736a73a0dadd14c6
pashkevich <pavelandreevsk@gmail.com>
2017-12-02 19:13:53 +0300

    Add Quoter interface and its implementation

--- src/main/java/quoters/Quoter.java
+++ src/main/java/quoters/Quoter.java
@@ -5,4 +5,5 @@ package quoters;
  */
 public interface Quoter {
 
+  public void sayQuote();
 }
---
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -0,0 +1,18 @@
+package quoters;
+
+/**
+ * Created by Andrey on 02.12.2017.
+ */
+public class TerminatorQuoter implements Quoter {
+
+  private String message;
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  @Override
+  public void sayQuote() {
+
+  }
+}
