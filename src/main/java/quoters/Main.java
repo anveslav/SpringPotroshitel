78e7987ba7df7aa5776c5d0841a02b25be038d06
pashkevich <pavelandreevsk@gmail.com>
2017-12-02 19:26:41 +0300

    Added Main class and executed app

---
+++ src/main/java/quoters/Main.java
@@ -0,0 +1,16 @@
+package quoters;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Andrey on 02.12.2017.
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+        "context.xml");
+    context.getBean(Quoter.class).sayQuote();
+  }
+
+}
