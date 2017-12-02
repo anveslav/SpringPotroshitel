fd10791fd398ef97b4c0d07a1ec070f162040f65
pashkevich <pavelandreevsk@gmail.com>
2017-12-02 20:54:28 +0300

    WIP, profiling works!

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -7,10 +7,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
         "context.xml");
-    context.getBean(Quoter.class).sayQuote();
+    while (true) {
+      Thread.sleep(100);
+      context.getBean(Quoter.class).sayQuote();
+    }
   }
 
 }
--- src/main/java/quoters/ProfilingHandlerBeanPostProcessor.java
+++ src/main/java/quoters/ProfilingHandlerBeanPostProcessor.java
@@ -1,20 +1,26 @@
 package quoters;
 
+import java.lang.management.ManagementFactory;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
-/**
- * Created by Andrey on 02.12.2017.
- */
 public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
 
   private Map<String, Class> map = new HashMap<String, Class>();
   private ProfilingController controller = new ProfilingController();
+
+  public ProfilingHandlerBeanPostProcessor() throws Exception {
+    MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+    mBeanServer.registerMBean(controller,new ObjectName("profiling","name", "controller"));
+
+  }
 
   @Override
   public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
@@ -33,12 +39,17 @@ public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
           new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-              System.out.println("Профилирую....");
-              long before = System.nanoTime();
-              Object retVal = method.invoke(o, args);
-              long after = System.nanoTime();
-              System.out.println("Всё!");
-              return retVal;
+              if (controller.isEnabled()) {
+                System.out.println("Профилирую...");
+                long before = System.nanoTime();
+                Object retVal = method.invoke(o, args);
+                long after = System.nanoTime();
+                System.out.println(after - before);
+                System.out.println("Всё!");
+                return retVal;
+              }else {
+                return method.invoke(o,args);
+              }
             }
           });
     }
