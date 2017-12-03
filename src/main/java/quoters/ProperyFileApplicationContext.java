a8c2e44ad78de8139bb635d2c08323149df62bdb
pashkevich <pavelandreevsk@gmail.com>
2017-12-03 18:58:32 +0300

    WIP, started work on creating own contextReader

---
+++ src/main/java/quoters/ProperyFileApplicationContext.java
@@ -0,0 +1,13 @@
+package quoters;
+
+import org.springframework.context.support.GenericApplicationContext;
+
+/**
+ * Created by Andrey on 03.12.2017.
+ */
+public class ProperyFileApplicationContext extends GenericApplicationContext {
+
+  public ProperyFileApplicationContext(String fileName) {
+
+  }
+}
